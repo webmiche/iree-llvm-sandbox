de3a54742ab4ac7982e8964d3e22c65408c9bb71
Michel Weber <michel.weber@inf.ethz.ch>
2022-10-11 13:59:32 +0200

    reset two queries

--- experimental/sql/TPC-H/q6.py
+++ experimental/sql/TPC-H/q6.py
@@ -10,8 +10,8 @@ def get_ibis_query(DATE="1994-01-01", DISCOUNT=0.06, QUANTITY=24):
   discount_max = round(DISCOUNT + 0.01, 2)
   q = q.filter([
       q.l_shipdate >= DATE, q.l_shipdate < add_date(DATE, dy=1),
-      q.l_discount >= ibis.literal(Decimal(discount_min), "decimal(32, 2)"),
-      q.l_discount <= ibis.literal(Decimal(discount_max), "decimal(32, 2)"),
+      q.l_discount.between(ibis.literal(discount_min, "decimal(32,2)"),
+                           ibis.literal(discount_max, "decimal(32,2)")),
       q.l_quantity < ibis.literal(QUANTITY, "int64")
   ])
   proj = q.projection(q.columns +
