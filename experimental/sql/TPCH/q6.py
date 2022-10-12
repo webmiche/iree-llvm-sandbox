e48584a44b699a0f7f2d6e7771ca80038f411a8c
Michel Weber <michel.weber@inf.ethz.ch>
2022-10-12 14:45:34 +0200

    update q6

--- experimental/sql/TPCH/q6.py
+++ experimental/sql/TPCH/q6.py
@@ -6,13 +6,14 @@ from decimal import Decimal
 def get_ibis_query(DATE="1994-01-01", DISCOUNT=0.06, QUANTITY=24):
   from TPCH.tpc_h_tables import lineitem
   q = lineitem
-  discount_min = int(round(DISCOUNT - 0.01, 2))
-  discount_max = int(round(DISCOUNT + 0.01, 2))
+  discount_min = round(DISCOUNT - 0.01, 2)
+  discount_max = round(DISCOUNT + 0.01, 2)
   q = q.filter([
       q.l_shipdate >= ibis.literal(DATE, "timestamp"),
       q.l_shipdate < ibis.literal(add_date(DATE, dy=1), "timestamp"),
-      q.l_discount.between(ibis.literal(discount_min, "decimal(32,2)"),
-                           ibis.literal(discount_max, "decimal(32,2)")),
+      q.l_discount.between(ibis.literal(Decimal(discount_min), "decimal(32,2)"),
+                           ibis.literal(Decimal(discount_max),
+                                        "decimal(32,2)")),
       q.l_quantity < ibis.literal(QUANTITY, "int64")
   ])
   proj = q.projection(q.columns +
